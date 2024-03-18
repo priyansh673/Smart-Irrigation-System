28f84ccadfda230fc53febc4690a1800a6e562cf
priyansh673 <121422165+priyansh673@users.noreply.github.com>
2024-03-19 01:09:39 +0530

    Update Irrigation_Device.ino

--- Irrigation_Device.ino
+++ Irrigation_Device.ino
@@ -1,4 +1,4 @@
-float model_coef[2] = {0.31252223, 0.18626435};  //Coeeficients calculated 
+float model_coef[2] = {0.31252223, -0.18626435};  //Coeeficients calculated 
 float model_intercept = -160.56781552;      
 
 void setup() {
@@ -15,19 +15,16 @@ void loop() {
   float tempValue = analogRead(A1);  // TMP36 temperature sensor
 
   // Apply the logistic regression model
-  
- 	float prediction = (model_coef[0] * moistureValue) + (model_coef[1] * tempValue) + model_intercept;
-
-
+   float linear_combination = model_coef[0] * moistureValue + model_coef[1] * tempValue + model_intercept;
+  float prediction = 1 / (1 + exp(-linear_combination));
   // Display results with single-color LED
-  if (prediction < 0.0) {
+  if (prediction < 0.5) {
     // Irrigation needed (Display HIGH)
     digitalWrite(13, HIGH);
   } else {
     // No irrigation needed (Display LOW)
     digitalWrite(13, LOW);
   }
-
   Serial.print("Moisture: ");
   Serial.print(moistureValue);
   Serial.print(", Temp: ");
@@ -37,4 +34,3 @@ void loop() {
 
   delay(1000);
 }
-
